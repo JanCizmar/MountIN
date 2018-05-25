6f0b9e312bf778390f8d8b2df171c87b9a22da22
jenik <cizmar@chlupac.com>
2018-05-25 10:35:03 +0200

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,59 @@
+"use strict";
+
+import React from 'react';
+import { HashRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+
+import { UserLoginView } from "./views/UserLoginView";
+import { UserSignupView } from "./views/UserSignupView";
+
+import UserService from "./services/UserService";
+
+
+export default class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            title: 'Movie Example App',
+            routes: [
+                { render: (props) => {
+                        return <div>It Works</div>;
+                        } , path: '/'},
+                { render: (props) => {
+                        if(UserService.isAuthenticated()) {
+                            return <div>Logged</div>;
+                        }
+                        else {
+                            return (<Redirect to={'/login'}/>)
+                        }} , path: '/edit/:id'},
+                { render: (props) => {
+                    if(UserService.isAuthenticated()) {
+                        return <div>Logged</div>;
+                    }
+                    else {
+                        return (<Redirect to={'/login'}/>)
+                    }}, path: '/add',},
+                { component: UserLoginView, path: '/login'},
+                { component: UserSignupView, path: '/register'}
+            ]
+        };
+    }
+
+    componentDidMount(){
+        document.title = this.state.title;
+    }
+
+    render() {
+        return(
+            <div>
+                <Router>
+                    <Switch>
+                        {this.state.routes.map((route, i) => (<Route key={i} {...route}/>) )}
+                    </Switch>
+                </Router>
+            </div>
+        );
+    }
+}
+
