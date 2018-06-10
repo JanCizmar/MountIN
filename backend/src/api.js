4bbc37a997169a4490a1314bb022f0cf8676574d
DESKTOP-KITL6EL\Stefan <ga86cuy@tum.de>
2018-06-11 00:37:07 +0200

    Deleted movie app specific code

--- backend/src/api.js
+++ backend/src/api.js
@@ -7,7 +7,6 @@ const helmet     = require('helmet');
 const middlewares = require('./middlewares');
 
 const auth  = require('./routes/auth');
-const movie = require('./routes/movie');
 const tour = require('./routes/tour');
 
 const api = express();
@@ -29,9 +28,6 @@ api.get('/', (req, res) => {
 
 // API routes
 api.use('/auth'  , auth);
-api.use('/movies', movie);
 api.use('/tours', tour); //todo: use the same name here?
-
-
 
 module.exports = api;
