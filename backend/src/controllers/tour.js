0f25d9813b288b34ff6c1f4d253e2faf0d3a3007
jenik <cizmar@chlupac.com>
2018-06-15 00:01:06 +0200

    Removed the dumb query arrays to query mapping, added conditions to $and.

--- backend/src/controllers/tour.js
+++ backend/src/controllers/tour.js
@@ -80,79 +80,50 @@ const list = (req, res) => {
 const search = (req, res) => {
 
     //res.send(JSON.stringify(req.query));
-    let query = {};
+    let query = {$and: []};
 
     if (req.query.difficulties !== undefined) {
-        let arrayDifficulties = req.query.difficulties.split(',');
-        //res.send(arrayDifficulties.length)
-        if (arrayDifficulties.length === 2) {
-            query.$or = [
-                {difficulty: arrayDifficulties[0]},
-                {difficulty: arrayDifficulties[1]}
-            ]
-        } else if (arrayDifficulties.length === 3) {
-            query.$or = [
-                {difficulty: arrayDifficulties[0]},
-                {difficulty: arrayDifficulties[1]},
-                {difficulty: arrayDifficulties[2]}
-            ]
-        } else if (arrayDifficulties.length === 4) {
-            query.$or = [
-                {difficulty: arrayDifficulties[0]},
-                {difficulty: arrayDifficulties[1]},
-                {difficulty: arrayDifficulties[2]},
-                {difficulty: arrayDifficulties[3]}
-            ]
-        } else {
-            query.difficulty = req.query.difficulties;
-        }
+        query.$and.push({
+            $or: req.query.difficulties.split(',').map((diff) => {
+                return {difficulty: diff}
+            })
+        });
     }
+
     if (req.query.dateAfter !== undefined) {
         query.date = {
             $gte: req.query.dateAfter,
         }
     }
+
     if (req.query.dateBefore !== undefined) {
         if (query.date === undefined) {
             query.date = {};
         }
         query.date.$lte = req.query.dateBefore;
     }
+
+
     if (req.query.activityTypes !== undefined) {
-        let arrayActivityTypes = req.query.activityTypes.split(',');
-        //res.send(arrayDifficulties.length)
-        if (arrayActivityTypes.length === 2) {
-            query.$or = [
-                {type: arrayActivityTypes[0]},
-                {type: arrayActivityTypes[1]}
-            ]
-        } else if (arrayActivityTypes.length === 3) {
-            query.$or = [
-                {type: arrayActivityTypes[0]},
-                {type: arrayActivityTypes[1]},
-                {type: arrayActivityTypes[2]}
-            ]
-        } else if (arrayActivityTypes.length === 4) {
-            query.$or = [
-                {type: arrayActivityTypes[0]},
-                {type: arrayActivityTypes[1]},
-                {type: arrayActivityTypes[2]},
-                {type: arrayActivityTypes[3]}
-            ]
-        } else {
-            query.type = req.query.activityTypes;
-        }
+        query.$and.push({
+            $or: req.query.activityTypes.split(',').map((type) => {
+                return {type}
+            })
+        });
     }
 
     if (req.query.guideTypes !== undefined) {
         let arrayGuideTypes = req.query.guideTypes.split(',');
 
         if (arrayGuideTypes.length === 1) {
-            console.log(typeof arrayGuideTypes[0]);
-            query.$or = [
-                {
-                    'creator.professional': arrayGuideTypes[0] === '1'
-                }]
+            query.$and.push({
+                    $or: [
+                        {
+                            'creator.professional': arrayGuideTypes[0] === '1'
+                        }
+                    ]
+                }
+            )
         }
     }
 
@@ -167,6 +138,7 @@ const search = (req, res) => {
             }
         }
     }
+
     if (req.query.price !== undefined) {
         let arrayPrices = req.query.price.split(',');
         query.cost = {
@@ -175,28 +147,12 @@ const search = (req, res) => {
         }
     }
 
-    //MyCollection._ensureIndex({'data.address.located':'2dsphere'});
-
-    //"lat=11.4505487&lng=48.256156&activityTypes=1,2&difficulties=2,5&guideTypes=0,1&priceMin=100&priceMax=200&dateBefore=...&dateAfter=..."
-    // var METERS_PER_MILE = 1609.34
-    // db.restaurants.find({ location: { $nearSphere: { $geometry: { type: "Point", coordinates: [ -73.93414657, 40.82302903 ] }, $maxDistance: 5 * METERS_PER_MILE } } })
-
-
-    //res.send(JSON.stringify(query));
-
     TourModel.find(query).exec()
         .then(tours => res.status(200).json(tours))
         .catch(error => res.status(500).json({
             error: 'Internal server error',
             message: error.message
         }));
-
-    // TourModel.findByIdAndUpdate(req.params.id,req.body,{ new: true, runValidators: true}).exec()
-    //     .then(tour => res.status(200).json(tour))
-    //     .catch(error => res.status(500).json({
-    //         error: 'Internal server error',
-    //         message: error.message
-    //     }));
 };
 
 
