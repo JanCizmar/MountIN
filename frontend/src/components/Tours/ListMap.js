88025816dd8d5d7114bb956b1710dd0038cad2c1
jenik <cizmar@chlupac.com>
2018-06-23 18:08:57 +0200

    Map in list basics

--- frontend/src/components/Tours/ListMap.js
+++ frontend/src/components/Tours/ListMap.js
@@ -1,12 +1,44 @@
-"use strict";
-
 import React from 'react';
-import Col from "react-bootstrap/es/Col";
+import {Marker} from "react-google-maps";
 
-export default function ListMap(props) {
-    return (
-        <Col></Col>
-    );
+const {compose, withProps, lifecycle} = require("recompose");
+const {
+    withScriptjs,
+    withGoogleMap,
+    GoogleMap,
+    DirectionsRenderer,
+} = require("react-google-maps");
+
+
+const ListMap = compose(
+    withProps({
+        googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyBiqph4VE1UPfoHwmBX7NYkFApXlngUVkI&v=3.exp&libraries=geometry,drawing,places",
+        loadingElement: <div style={{height: `100%`}}/>,
+        containerElement: <div style={{height: `400px`}}/>,
+        mapElement: <div style={{height: `100%`}}/>
+    }),
+    withScriptjs,
+    withGoogleMap,
+)(props => {
+        return <GoogleMap
+            defaultZoom={9}
+            defaultCenter={new google.maps.LatLng(48.150040, 11.545055)}
+        >
+            <Markers {...props.tours}/>
+        </GoogleMap>;
+    }
+);
+
+export default ListMap;
+
+/** @return mixed */
+function Markers(props) {
+
+
+    if (props.lat !== undefined && props.lng !== undefined) {
+        return <Marker
+            position={{lat: 12, lng: 15}}
+        />
+    }
+    return null;
 }
-
-ListMap.propTypes = {};
